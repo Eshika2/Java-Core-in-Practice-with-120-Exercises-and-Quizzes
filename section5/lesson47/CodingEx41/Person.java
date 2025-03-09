1890f76d9a3d6ab1eadf8892fdb1ba065adf1126
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2025-03-09 19:23:24 +0530

    Update Person.java

--- section5/lesson47/CodingEx41/Person.java
+++ section5/lesson47/CodingEx41/Person.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Person implements Comparable <Person>{
 
     private String name;
-    private int age;
+     int age;
 
     public Person(String name, int age) {
         this.name = name;
